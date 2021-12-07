bc56c3831b4346cae46c38be824c98f8cfa37d75
scut_gzic <253604213@qq.com>
2021-12-07 16:47:59 +0800

    添加项目文件。

---
+++ sEMG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sEMG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
